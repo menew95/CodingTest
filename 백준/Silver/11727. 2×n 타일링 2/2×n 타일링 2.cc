66a12059108eb67748e05daa1487ca7736766e88
menew95 <64944509+menew95@users.noreply.github.com>
2023-11-17 16:06:25 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n)
+{
+	vector<int> _dp(n);
+
+	_dp[0] = 1;
+	_dp[1] = 3;
+
+	for (int i = 2; i < n; i++)
+	{
+		_dp[i] = (_dp[i - 1] + _dp[i - 2] * 2) % 10007;
+	}
+
+	return _dp.back();
+
+}
+
+int main()
+{
+	int _n;
+
+	cin >> _n;
+
+	cout << solution(_n) << endl;
+}
